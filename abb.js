13cc17468c1482602d97942ddcfa3300189894f9
medetisrafilov <israfilovmedet52@gmail.com>
2024-11-10 01:01:46 -1200

    home work

---
+++ abb.js
@@ -0,0 +1,277 @@
+let a = 90
+// if (a == 10);
+switch(a){
+    case 1:
+        console.log("bir")
+    break
+    case 2:
+        console.log("iki")
+    break
+    case 3:
+        console.log("üç")
+    break
+    case 4:
+        console.log("dört")
+    break
+    case 5:
+        console.log("beş")
+    break
+    case 6:
+        console.log("altı")
+    break
+    case 7:
+        console.log("yeddi")
+    break
+    case 8:
+        console.log("səggiz")
+    break
+    case 9:
+        console.log("doqquz")
+    break
+    case 10:
+        console.log("on")
+    break
+    case 11:
+        console.log("on bir")
+    break
+    case 12:
+        console.log("on iki")
+    break
+    case 13:
+        console.log("on üç")
+    break
+    case 14:
+        console.log("on dört")
+    break
+    case 15:
+        console.log("on beş")
+    break
+    case 16:
+        console.log("on altı")
+    break
+    case 17:
+        console.log("on yeddi")
+    break
+    case 18:
+        console.log("on səggiz")
+    break
+    case 19:
+        console.log("on doqquz")
+    break
+    case 20:
+        console.log("iyirmi")
+    break
+    case 21:
+        console.log("iyirmi bir")
+    break
+    case 22:
+        console.log(" iyirmi iki")
+    break
+    case 23:
+        console.log("iyirmi üç")
+    break
+    case 24:
+        console.log("iyirmi dört")
+    break
+    case 25:
+        console.log("iyirmi beş")
+    break
+    case 26:
+        console.log("iyirmi altı")
+    break
+    case 27:
+        console.log("iyirmi yeddi")
+    break
+    case 28:
+        console.log("iyirmi səggiz")
+    break
+    case 29:
+        console.log("iyirmi doqquz")
+    break
+    case 30:
+        console.log("otuz")
+    break
+    case 31:
+        console.log("otuz bir")
+    break
+    case 32:
+        console.log("otuz iki")
+    break
+    case 33:
+        console.log("otuz üç")
+    break
+    case 34:
+        console.log("otuz dört")
+    break
+    case 35:
+        console.log("otuz beş")
+    break
+    case 36:
+        console.log("otuz altı")
+    break
+    case 37:
+        console.log("otuz yeddi")
+    break
+    case 38:
+        console.log("otuz səggiz")
+    break
+    case 39:
+        console.log("otuz doqquz")
+    break
+    case 40:
+        console.log("qırx")
+    break
+    case 41:
+        console.log("qırx bir")
+    break
+    case 42:
+        console.log("qırx iki")
+    break
+    case 43:
+        console.log("qırx üç")
+    break
+    case 44:
+        console.log("qırx dört")
+    break
+    case 45:
+        console.log("qırx beş")
+    break
+    case 46:
+        console.log("qırx altı")
+    break
+    case 47:
+        console.log("qırx yeddi")
+    break
+    case 48:
+        console.log("qırx səggiz")
+    break
+    case 49:
+        console.log("qırx doqquz")
+    break
+    case 50:
+        console.log("əlli")
+    break
+    case 51:
+        console.log("əlli bir")
+    break
+    case 52:
+        console.log("əlli iki")
+    break
+    case 53:
+        console.log("əlli üç")
+    break
+    case 54:
+        console.log("əlli dört")
+    break
+    case 55:
+        console.log("əlli beş")
+    break
+    case 56:
+        console.log("əlli altı")
+    break
+    case 57:
+        console.log("əlli yeddi")
+    break
+    case 58:
+        console.log("əlli səggiz")
+    break
+    case 59:
+        console.log("əlli doqquz")
+    break
+    case 60:
+        console.log("altımış")
+    break
+    case 61:
+        console.log("altımış bir")
+    break
+    case 62:
+        console.log("altımış iki")
+    break
+    case 63:
+        console.log("altımış üç")
+    break
+    case 64:
+        console.log("altımış dört")
+    break
+    case 65:
+        console.log("altımış beş")
+    break
+    case 66:
+        console.log("altımış altı")
+    break
+    case 67:
+        console.log("altımış yeddi")
+    break
+    case 68:
+        console.log("altımış səggiz")
+    break
+    case 69:
+        console.log("altımış doqquz")
+    break
+    case 70:
+        console.log("yetmiş")
+    break
+    case 71:
+        console.log("yetmiş bir")
+    break
+    case 72:
+        console.log("yetmiş iki")
+    break
+    case 73:
+        console.log("yetmiş üç")
+    break
+    case 74:
+        console.log("yetmiş dört")
+    break
+    case 75:
+        console.log("yetmiş beş")
+    break
+    case 76:
+        console.log("yetmiş altı")
+    break
+    case 77:
+        console.log("yetmiş yeddi")
+    break
+    case 78:
+        console.log("yetmiş səggiz")
+    break
+    case 79:
+        console.log("yetmiş doqquz")
+    break
+    case 80:
+        console.log("həşdad")
+    break
+    case 81:
+        console.log("həşdad bir")
+    break
+    case 82:
+        console.log("həşdad iki")
+    break
+    case 83:
+        console.log("həşdad üç")
+    break
+    case 84:
+        console.log("həşdad dört")
+    break
+    case 85:
+        console.log("həşdad beş")
+    break
+    case 86:
+        console.log("həşdad altı")
+    break
+    case 87:
+        console.log("həşdad yeddi")
+    break
+    case 88:
+        console.log("həşdad səggiz")
+    break
+    case 89:
+        console.log("həşdad doqquz")
+    break
+    case 90:
+        console.log("doxsan")
+    break
+
+    consoleşlog("yalnış Dəyər")
+
+}
